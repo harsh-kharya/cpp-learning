7a659a58aea446319440228b31ac44c0fbd2bb58
Harsh Kharya <harshkharya20@gmail.com>
2025-06-24 22:51:52 +0530

    Add hello_world.cpp to mark the beginning of my journey!

---
+++ basics/hello_world.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Imports the "iostream" header file used for input & output.
+
+int main() { //Initializes the "main" function.
+    std::cout << "Hello, World!" <<  std::endl;
+    return 0;
+}
