4eb57d7919947767935e304cfd9468c21973eff4
Harsh Kharya <harshkharya20@gmail.com>
2025-06-24 22:58:29 +0530

    Initial commit

--- README.md
+++
@@ -1,7 +0,0 @@
-# C++ Learning Journey 🚀
-
-This repo contains my practice files while learning C++.
-
-## 👨‍💻 Author
-
-Harsh, Class 12 PCM student aspiring to become a software developer.
--- basics/hello_world.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream> // Imports the "iostream" header file used for input & output.
-
-int main() { //Initializes the "main" function.
-    std::cout << "Hello, World!" <<  std::endl;
-    return 0;
-}
